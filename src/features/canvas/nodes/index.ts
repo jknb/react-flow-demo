62825129c6af02ff375fb4fd2f0613f5ca080abb
jknb <alvertischris@gmail.com>
2025-11-24 12:37:42 +0200

    feat(nodes): add barrel export for node components
    - Created index.ts to export all node components from a single entry point
    - Exports StartNode, DecisionNode, ServiceNode, EndNode, and NodeLayout
    - Simplifies imports and improves code organization

---
+++ src/features/canvas/nodes/index.ts
@@ -0,0 +1,5 @@
+export { default as StartNode } from './StartNode';
+export { default as DecisionNode } from './DecisionNode';
+export { default as ServiceNode } from './ServiceNode';
+export { default as EndNode } from './EndNode';
+export { default as NodeLayout } from './NodeLayout';
